c7875ac4e0658eb3d9ca7735037a932b98eaa0e0
Liam Mercier <LiamMercier@proton.me>
2025-10-17 15:59:39 -0700

    Asset resolver functions

---
+++ src/protocol/asset-resolver.h
@@ -0,0 +1,215 @@
+#pragma once
+
+#include <filesystem>
+#include <string>
+#include <cstdlib>
+#include <system_error>
+
+namespace AppAssets {
+
+    constexpr const char * application_name = "SilentTanks";
+
+    inline std::filesystem::path join(const std::filesystem::path & base,
+                                      const std::string & appended)
+    {
+        if (base.empty())
+        {
+            return {};
+        }
+
+        return base / appended;
+    }
+
+    inline std::filesystem::path get_env_path(const char * name)
+    {
+        // If we didn't get nullptr back return the path
+        if (const char * v = std::getenv(name))
+        {
+            // prevent empty strings like XDG_DATA_HOME=""
+            if (v[0] != '\0')
+            {
+                return std::filesystem::path(v);
+            }
+        }
+
+        return {};
+    }
+
+    inline std::filesystem::path get_assets_dir()
+    {
+
+#ifdef _WIN32
+        // Search for the local app data folder in the environment.
+        auto pf = get_env_path("LOCALAPPDATA");
+
+        // If this doesn't exist, asset lookup fails.
+        if (pf.empty())
+        {
+            return {};
+        }
+
+        return pf / application_name;
+
+#else
+        // On linux, we want to use ~/.local/share
+
+        // Start by looking for the XDG_DATA_HOME env variable.
+        auto xdg = get_env_path("XDG_DATA_HOME");
+        if (!xdg.empty())
+        {
+            return xdg / application_name;
+        }
+
+        // Try manual home if xdg isn't defined.
+        auto home = get_env_path("HOME");
+        if (!home.empty())
+        {
+            return home / ".local" / "share" / application_name;
+        }
+
+        return {};
+
+#endif
+    }
+
+    inline bool fs_safe_exists(const std::filesystem::path & p)
+    {
+        if (p.empty())
+        {
+            return false;
+        }
+
+        std::error_code ec;
+        bool exists = std::filesystem::exists(p, ec);
+
+        // If ec was set, return false.
+        return (exists && !ec);
+    }
+
+    // Take a filename and resolve an asset, returning an empty path if it failed.
+    inline std::filesystem::path resolve_asset(const std::string & filename)
+    {
+        // If we set an env path for this application, use this instead.
+        {
+            auto override_dir = get_env_path("SILENTTANKS_ASSET_DIR");
+            if (!override_dir.empty())
+            {
+                auto candidate = join(override_dir, filename);
+                if (fs_safe_exists(candidate))
+                {
+                    return candidate;
+                }
+
+                return {};
+            }
+        }
+
+        // Otherwise, if a dev mode flag was set use this
+#if defined(DEV_BUILD)
+        {
+            std::error_code ec;
+            std::filesystem::path cwd = std::filesystem::current_path(ec);
+
+            // Prevent using cwd if empty or does not exist.
+            if (!ec && !cwd.empty())
+            {
+                auto devpath = cwd / filename;
+
+                if (fs_safe_exists(devpath))
+                {
+                    return devpath;
+                }
+            }
+
+            return {};
+        }
+#else
+
+        // For release, grab the asset folder and join.
+        {
+            std::filesystem::path assets_dir = get_assets_dir();
+
+            if (assets_dir.empty())
+            {
+                return {};
+            }
+
+            auto candidate = join(assets_dir, filename);
+
+            if (fs_safe_exists(candidate))
+            {
+                return candidate;
+            }
+
+            return {};
+        }
+#endif
+
+    }
+
+    // Go through each expected location and check the asset can be resolved.
+    // Otherwise, fail fast and report back to the user.
+    //
+    // This function is called at the start of the server.
+    inline bool assert_server_assets_resolvable()
+    {
+        bool resolvable = true;
+
+        std::vector<std::string> asset_names = {
+            // Server TLS files
+            "certs/server.crt",
+            "certs/server.key",
+
+            // List of maps
+            "mapfile.txt"
+        };
+
+        for (const auto & filename : asset_names)
+        {
+            std::filesystem::path path = resolve_asset(filename);
+
+            if (path.empty())
+            {
+                std::cerr << "Failed to resolve "
+                          << filename
+                          << " (resolved to "
+                          << path
+                          << ")\n";
+
+                resolvable = false;
+            }
+        }
+
+        return resolvable;
+    }
+
+    // Same case, but for client specific files and directories.
+    inline bool assert_client_assets_resolvable()
+    {
+        bool resolvable = true;
+
+        std::vector<std::string> asset_names = {
+            "certs",
+            "envs"
+        };
+
+        for (const auto & filename : asset_names)
+        {
+            std::filesystem::path path = resolve_asset(filename);
+
+            if (path.empty())
+            {
+                std::cerr << "Failed to resolve "
+                          << filename
+                          << " (resolved to "
+                          << path
+                          << ")\n";
+
+                resolvable = false;
+            }
+        }
+
+        return resolvable;
+    }
+
+}
